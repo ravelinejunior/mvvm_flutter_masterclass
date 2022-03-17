cde4fad4417bdc930dd4929d5e118b5461d8f8b2
ravelinejunior <junior.raveline@gmail.com>
2022-03-17 20:20:52 -0300

    Data Layer - Creating Mappers and Applying Null Safety

---
+++ lib/data/mapper/mapper.dart
@@ -0,0 +1,38 @@
+import 'package:mvvm_flutter_masterclass/app/extensions.dart';
+import 'package:mvvm_flutter_masterclass/data/response/responses.dart';
+import 'package:mvvm_flutter_masterclass/domain/authentication_model.dart';
+import 'package:mvvm_flutter_masterclass/domain/contacts_model.dart';
+import 'package:mvvm_flutter_masterclass/domain/customer_model.dart';
+
+const String emptyString = "";
+const int zeroInt = 0;
+
+//convert the responses into a non nullable object (model)
+
+extension CustomerResponseMapper on CostumerResponse? {
+  CustomerModel toDomain() {
+    return CustomerModel(
+      id: this?.id?.orEmpty() ?? emptyString,
+      name: this?.name?.orEmpty() ?? emptyString,
+      numOfNotifications: this?.numOfNotifications?.orZero() ?? zeroInt,
+    );
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse? {
+  ContactsModel toDomain() {
+    return ContactsModel(
+      email: this?.email?.orEmpty() ?? emptyString,
+      phone: this?.phone?.orEmpty() ?? emptyString,
+      link: this?.link?.orEmpty() ?? emptyString,
+    );
+  }
+}
+
+extension AuthenticationResponseMapper on AuthenticationResponse? {
+  AuthenticationModel toDomain() {
+    return AuthenticationModel(
+        contactsModel: this?.contactsResponse?.toDomain(),
+        customerModel: this?.costumerResponse?.toDomain());
+  }
+}
--- lib/domain/authentication_model.dart
+++ lib/domain/authentication_model.dart
@@ -2,8 +2,8 @@ import 'package:mvvm_flutter_masterclass/domain/contacts_model.dart';
 import 'package:mvvm_flutter_masterclass/domain/customer_model.dart';
 
 class AuthenticationModel {
-  CustomerModel customerModel;
-  ContactsModel contactsModel;
+  CustomerModel? customerModel;
+  ContactsModel? contactsModel;
   AuthenticationModel({
     required this.customerModel,
     required this.contactsModel,
